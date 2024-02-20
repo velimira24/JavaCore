3517c4c5bf32b17c1679edb92ce5c7d0f2bd2858
Velimira <revekka-best@mail.ru>
2024-02-20 14:20:55 +0300

    JJHW

---
+++ JavaJunior/lesson5/chatserver/server/ClientManager.java
@@ -0,0 +1,87 @@
+package JavaJunior.lesson5.chatserver.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientManager implements Runnable {
+
+    private final Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String name;
+
+    public final static ArrayList<ClientManager> clients = new ArrayList<>();
+
+    public ClientManager(Socket socket) {
+        this.socket = socket;
+        try {
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            name = bufferedReader.readLine();
+            clients.add(this);
+            System.out.println(name + " вошел в чат");
+            broadcastMessage("Server: " + name + " вошел в чат.");
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+
+    }
+
+    @Override
+    public void run() {
+        String massageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                massageFromClient = bufferedReader.readLine();
+                broadcastMessage(massageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    private void broadcastMessage(String message) {
+        for (ClientManager client : clients) {
+            try {
+                if (!client.name.equals(name)) {
+                    client.bufferedWriter.write(message);
+                    client.bufferedWriter.newLine();
+                    client.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    private void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        // Удаление клиента
+        removeClient();
+        try {
+            // Завершаем работу буфера на чтение данных
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            // Завершаем работу буфера для записи данных
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            // Закрытие соединения с клиентским сокетом
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void removeClient() {
+        clients.remove(this);
+        System.out.println(name + " покинул чат.");
+        broadcastMessage("Server: " + name + " покинул чат.");
+    }
+
+}
---
+++ JavaJunior/lesson5/exm/Main.java
@@ -0,0 +1,7 @@
+package JavaJunior.lesson5.exm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет");
+    }
+}
